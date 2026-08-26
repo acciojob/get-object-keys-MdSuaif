fcd3b72d80f885d6472b890bc79bc826c851eac4
MdSuaif <mdsuaif00@gmail.com>
2026-08-26 12:17:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const studen = {
+	name : "suaif",
+	age : 22,
+	city : "Hyderabad"
+}
+function getKeys(obj) {
+return object.keys(obj)	
+}
+console.log(getKeys(student));
